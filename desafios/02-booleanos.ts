4a4cbaec65af353bcf037d9edf7109fa1c430dfa
gabriel.melo <gabriel.melo@rethink.dev>
2025-05-20 15:50:51 -0300

    feat: exercise 2 - improve variable naming

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -4,28 +4,28 @@ const user = {
   name: 'Diego Fernandes',
   height: 190,
   hasTicket: true,
+};
+
+const minimumHeightToRide = 130;
+
+const currentHour = new Date().getHours();
+
+const isParkOpen = currentHour > 9 && currentHour < 18;
+
+if (!isParkOpen) {
+  throw new Error('O parque está fechado!');
 }
 
-const necessaryHeight = 130
+const hasValidTicket = user.hasTicket;
 
-const currentHour = new Date().getHours()
-
-const parkOpen = currentHour > 9 && currentHour < 18
-
-if (!parkOpen) {
-  throw new Error('O parque está fechado!')
+if (!hasValidTicket) {
+  throw new Error('O Diego não possui um bilhete para entrar no parque!');
 }
 
-const ticket = user.hasTicket
+const isTallEnough = user.height > minimumHeightToRide;
 
-if (!ticket) {
-  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+if (!isTallEnough) {
+  throw new Error('O Diego não pode entrar no brinquedo!');
 }
 
-const enterToy = user.height > necessaryHeight
-
-if (!enterToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
-} 
-
-console.log('O Diego se divertiu muito! :)')
+console.log('O Diego se divertiu muito! :)');
